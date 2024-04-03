fddb9b41e85bbc81faaf00f5c25f9cb23f8c4966
João Otávio Leites Matuszevski <joaoolmdev@gmail.com>
2024-04-03 14:46:16 -0300

    improve: addin try in events repository

--- src/models/repository/events_repository_test.py
+++ src/models/repository/events_repository_test.py
@@ -17,10 +17,10 @@ def test_insert_event():
     response = events_repository.insert_event(event)
     print(response)
 
-@pytest.mark.skip(reason="nao nescessita")
+
 def test_get_event_by_id():
-    event_id = "meu-uuid2"
+    event_id = "meu-uuid23456"
     events_repository = EventsRepository()
     response = events_repository.get_event_by_id(event_id)
     print(response)
-    print(response.title)
+    #print(response.title)
